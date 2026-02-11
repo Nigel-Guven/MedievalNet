00c3c05c40b6a940c45ee3e26e90907b9b82c01f
Nigel Guven <nigelguven@gmail.com>
2026-02-11 17:52:41 +0000

    Map Rendering Service complete

--- world-map-renderer/src/map_renderer.py
+++ world-map-renderer/src/map_renderer.py
@@ -2,8 +2,8 @@ from PIL import Image, ImageDraw
 from consts import FACTION_COLOR
 import os
 
-TEMPLATE_PATH = "./templates/map_regions.tga"
-OUTPUT_DIR = "./output"
+TEMPLATE_PATH  = "./templates/map_regions.tga"
+OUTPUT_DIR  = "./output"
 
 def render_world_map(world_id, factions):
 
@@ -33,8 +33,13 @@ def render_world_map(world_id, factions):
                 if current_color in region_color_map:
                     pixels[x, y] = region_color_map[current_color]
         print("We Get to HEREB!!!!!")
-        os.makedirs(OUTPUT_DIR, exist_ok=True)
-        path = os.path.join(OUTPUT_DIR, f"world_{world_id}.png")
-        image.save(path)
 
-        print(f"Rendered map saved to {path}")
+        file_name = f"{world_id}.tga"
+
+        world_folder = os.path.join(OUTPUT_DIR, str(world_id))
+        os.makedirs(world_folder, exist_ok=True)
+
+        file_path = os.path.join(world_folder, file_name)
+
+        image.save(file_path) 
+        print(f"Map saved to {file_path}")
